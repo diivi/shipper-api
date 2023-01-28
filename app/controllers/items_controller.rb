c729030c2d0ff3d6ae8879f983e86c2704ac22b6
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-01-28 07:53:39 +0530

    items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,7 +18,7 @@ class ItemsController < ApplicationController
   def create
     @item = @warehouse.items.build(item_params)
     if @item.save
-      render json: @item, status: :created, location: @item
+      render json: @item, status: :created
     else
       render json: @item.errors, status: :unprocessable_entity
     end
