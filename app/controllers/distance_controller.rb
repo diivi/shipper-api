6bba0191246cba8fbf51be50f143861cf659ae74
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-01-28 12:15:17 +0530

    distance api responses edit

--- app/controllers/distance_controller.rb
+++ app/controllers/distance_controller.rb
@@ -2,6 +2,10 @@ class DistanceController < ApplicationController
     def distance_to_india
         origin = params[:origin]
         distance = Distance.find_by(country: origin)
-        render json: { distance: distance.distance }
+        unless distance.nil?
+            render json: {distance: distance.distance}, status: :ok
+        else
+            render json: {error: "Distance not found"}, status: :not_found
+        end            
     end
 end
